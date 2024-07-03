0417e0a1e97d94b0b3b958e187df5b783bf53107
eitanVIP <eitangrimblat@gmail.com>
2024-07-03 18:00:18 +0300

    Join menu and font

---
+++ ClashFPS/Assets/Scripts/NetworkUI.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkUI : MonoBehaviour
+{
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+}
