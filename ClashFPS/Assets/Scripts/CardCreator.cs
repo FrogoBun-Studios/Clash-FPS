23701838c9695939467215e9707a4edd95744732
eitanVIP <eitangrimblat@gmail.com>
2024-08-18 23:15:28 +0300

    Card Creator & Remover
    iwannadie

---
+++ ClashFPS/Assets/Scripts/CardCreator.cs
@@ -0,0 +1,265 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+using Unity.Netcode;
+using Unity.Multiplayer.Samples.Utilities.ClientAuthority;
+using System.IO;
+using System.Linq;
+
+public class CardCreator : EditorWindow
+{
+    private string name = "";
+    private GameObject model = null;
+    private AnimationClip[] animations = new AnimationClip[8];
+    private Avatar avatar = null;
+    private NetworkBehaviour script = null;
+    private GameObject modelPrefab = null;
+    private GameObject cardPrefab = null;
+
+    [MenuItem("Window/Card Creator")]
+    public static void ShowWindow(){
+        GetWindow<CardCreator>("Card Creator");
+    }
+
+    private void GetInfo(){
+        name = EditorGUILayout.TextField("Card name", name);
+        model = (GameObject)EditorGUILayout.ObjectField("3D Model(Blender File)", model, typeof(GameObject), false);
+
+        GUILayout.Label("Card Animations", EditorStyles.boldLabel);
+        animations[0] = (AnimationClip)EditorGUILayout.ObjectField("Idle", animations[0], typeof(AnimationClip), false);
+        animations[1] = (AnimationClip)EditorGUILayout.ObjectField("Running", animations[1], typeof(AnimationClip), false);
+        animations[2] = (AnimationClip)EditorGUILayout.ObjectField("Jump", animations[2], typeof(AnimationClip), false);
+        animations[3] = (AnimationClip)EditorGUILayout.ObjectField("Attack", animations[3], typeof(AnimationClip), false);
+        animations[4] = (AnimationClip)EditorGUILayout.ObjectField("RunAttack", animations[4], typeof(AnimationClip), false);
+        animations[5] = (AnimationClip)EditorGUILayout.ObjectField("RunJump", animations[5], typeof(AnimationClip), false);
+        animations[6] = (AnimationClip)EditorGUILayout.ObjectField("RunJumpAttack", animations[6], typeof(AnimationClip), false);
+        animations[7] = (AnimationClip)EditorGUILayout.ObjectField("Death", animations[7], typeof(AnimationClip), false);
+
+        avatar = (Avatar)EditorGUILayout.ObjectField("Animator Avatar", avatar, typeof(Avatar), false);
+    }
+
+    private void CreateFolder(){
+        string folderPath = $"Assets/Resources/{name}";
+
+        if (!AssetDatabase.IsValidFolder(folderPath)){
+            AssetDatabase.CreateFolder("Assets/Resources", $"{name}");
+            AssetDatabase.Refresh();
+            Debug.Log("Card Creator: Folder created at " + folderPath);
+        }
+        else
+            Debug.LogError("Card Creator: Folder already exists at " + folderPath);
+    }
+
+    private void CreateAnimator(){
+        bool success = AssetDatabase.CopyAsset("Assets/Resources/Valkyrie/Animator.controller", $"Assets/Resources/{name}/Animator.controller");
+        if (success)
+            Debug.Log("Card Creator: Animator controller created.");
+        else
+            Debug.LogError("Card Creator: Failed to create animator controller.");
+
+        AssetDatabase.Refresh();
+
+        AnimatorController animator = Resources.Load<AnimatorController>($"{name}/Animator");
+
+        animator.layers[0].stateMachine.states[0].state.motion = animations[0];
+        animator.layers[0].stateMachine.states[1].state.motion = animations[7];
+        animator.layers[0].stateMachine.states[2].state.motion = animations[3];
+        animator.layers[0].stateMachine.states[3].state.motion = animations[2];
+        animator.layers[0].stateMachine.states[4].state.motion = animations[1];
+        animator.layers[0].stateMachine.states[5].state.motion = animations[6];
+        animator.layers[0].stateMachine.states[6].state.motion = animations[5];
+        animator.layers[0].stateMachine.states[7].state.motion = animations[4];
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        Debug.Log("Card Creator: Animator controller set.");
+    }
+
+    private void CreateModelPrefab(){
+        modelPrefab = PrefabUtility.SaveAsPrefabAsset(model, AssetDatabase.GenerateUniqueAssetPath($"Assets/Resources/{name}/ModelPrefab.prefab"));
+        AssetDatabase.Refresh();
+
+        Debug.Log("Card Creator: Model prefab created.");
+
+        modelPrefab.GetComponent<Animator>().runtimeAnimatorController = Resources.Load<AnimatorController>($"{name}/Animator");
+        modelPrefab.GetComponent<Animator>().avatar = avatar;
+        modelPrefab.AddComponent<NetworkObject>();
+        modelPrefab.AddComponent<ClientNetworkTransform>().SyncRotAngleX = false;
+        modelPrefab.GetComponent<ClientNetworkTransform>().SyncRotAngleZ = false;
+        modelPrefab.GetComponent<ClientNetworkTransform>().SyncScaleX = false;
+        modelPrefab.GetComponent<ClientNetworkTransform>().SyncScaleY = false;
+        modelPrefab.GetComponent<ClientNetworkTransform>().SyncScaleZ = false;
+        modelPrefab.AddComponent<ClientNetworkAnimator>().Animator = modelPrefab.GetComponent<Animator>();
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        Debug.Log("Card Creator: Model prefab set.");
+    }
+
+    private void CreateCardScript(){
+        string scriptTemplate = 
+@"using UnityEngine;
+
+public class " + name + @"Card : Card
+{
+    public override void StartCard(Transform player)
+    {
+        base.StartCard(player, new CardParams(
+            health: CardParamHelper.Health.Heavy,
+            damage: CardParamHelper.Damage.MediumHigh,
+            speed: CardParamHelper.Speed.Medium,
+            JumpStrength: CardParamHelper.JumpStrength.MediumHigh,
+            jumps: 2,
+            elixer: 4,
+            flying: false,
+            AttackRate: CardParamHelper.AttackRate.Medium,
+            side: Side.Blue
+        ), """ + name + @""");
+    }
+}";
+
+        File.WriteAllText($"Assets/Scripts/Cards/{name}Card.cs", scriptTemplate);
+        AssetDatabase.Refresh();
+
+        Debug.Log($"Card Creator: {name}Card.cs created.");
+    }
+
+    private void CreateCardPrefab(){
+        GameObject cardPrefab = new GameObject();
+        cardPrefab.AddComponent<NetworkObject>();
+
+        this.cardPrefab = PrefabUtility.SaveAsPrefabAsset(cardPrefab, AssetDatabase.GenerateUniqueAssetPath($"Assets/Resources/{name}/{name}Card.prefab"));
+        Debug.Log($"Card Creator: {name}Card.prefab created.");
+
+        Undo.DestroyObjectImmediate(cardPrefab);
+        Debug.Log($"Card Creator: Deleted \"New GameObject\" from scene, if something important was deleted, you can undo with ctrl-z");
+
+        AssetDatabase.Refresh();
+
+        Debug.Log($"Card Creator: {name}Card.prefab created.");
+        Debug.Log($"Card Creator: Don't forget to add {name}Card.cs to {name}Card.prefab.");
+    }
+
+    private void EditNetworkPrefabs(){
+        Resources.Load<NetworkPrefabsList>("NetworkPrefabs").Add(new NetworkPrefab{ Prefab = modelPrefab });
+        Resources.Load<NetworkPrefabsList>("NetworkPrefabs").Add(new NetworkPrefab{ Prefab = cardPrefab });
+    }
+
+    private void EditCardTypes(){
+        string path = $"Assets/Scripts/Helpers/CardTypes.cs";
+
+        string scriptContent = File.ReadAllText(path);
+        
+        int lastConstIndex = scriptContent.LastIndexOf("public const string ");
+        int insertPosition = scriptContent.IndexOf(';', lastConstIndex) + 1;
+        string newConstant = $"\n    public const string {name} = \"{name}\";";
+
+        scriptContent = scriptContent.Insert(insertPosition, newConstant);
+
+        File.WriteAllText(path, scriptContent);
+        AssetDatabase.Refresh();
+
+        Debug.Log("Card Creator: CardTypes edited successfully.");
+    }
+
+    private void CreateCard(){
+        CreateFolder();
+        CreateAnimator();
+        CreateModelPrefab();
+        CreateCardScript();
+        CreateCardPrefab();
+        EditNetworkPrefabs();
+        EditCardTypes();
+    }
+
+    private void RemoveFolder(){
+        string folderPath = $"Assets/Resources/{name}";
+
+        if (Directory.Exists(folderPath))
+        {
+            Directory.Delete(folderPath, true);
+            Debug.Log($"Card Creator: Card folder and its contents deleted at path {folderPath}.");
+        }
+        else
+            Debug.LogWarning($"Card folder does not exist at path {folderPath}.");
+
+        AssetDatabase.Refresh();
+    }
+
+    private void RemoveCardScript(){
+        string path = $"Assets/Scripts/Cards/{name}Card.cs";
+        File.Delete(path);
+
+        Debug.Log("Card Creator: Card script deleted.");
+    }
+
+    private void RemoveFromNetworkPrefabs(){
+        NetworkPrefabsList list = Resources.Load<NetworkPrefabsList>("NetworkPrefabs");
+        list.Remove(list.PrefabList.ToList().Find(x => x.Prefab == modelPrefab));
+        list.Remove(list.PrefabList.ToList().Find(x => x.Prefab == cardPrefab));
+
+        Debug.Log("Card Creator: Prefabs removed from NetworkPrefabs list.");
+    }
+
+    private void RemoveCardType(){
+        string path = $"Assets/Scripts/Helpers/CardTypes.cs";
+        string scriptContent = File.ReadAllText(path);
+
+        int lastConstIndex = scriptContent.LastIndexOf("public const string ");
+        int endOfLineIndex = scriptContent.IndexOf(';', lastConstIndex) + 1;
+
+        scriptContent = scriptContent.Remove(lastConstIndex, endOfLineIndex - lastConstIndex);
+
+        File.WriteAllText(path, scriptContent);
+        AssetDatabase.Refresh();
+
+        Debug.Log("Card Creator: CardTypes edited successfully.");
+    }
+
+    private void RemoveCard(){
+        RemoveFolder();
+        RemoveCardScript();
+        RemoveFromNetworkPrefabs();
+        RemoveCardType();
+    }
+
+    private void OnGUI(){
+        GUIStyle headlineStyle = new GUIStyle(GUI.skin.label)
+        {
+            padding = new RectOffset(10, 10, 10, 10),
+            fontSize = 20,
+            fontStyle = FontStyle.Bold,
+            alignment = TextAnchor.MiddleCenter
+        };
+
+        GUILayout.Label("Card Creator", headlineStyle);
+
+        GUILayout.Space(20);
+
+        GetInfo();
+
+        GUILayout.Space(20);
+
+        if(GUILayout.Button("Create Card")){
+            CreateCard();
+
+            GUILayout.Label($"Please attach {name}Card.cs to {name}Card.prefab in Assets/Resources/{name}.");
+        }
+
+        GUILayout.Space(40);
+
+        GUILayout.Label("Card Remover", headlineStyle);
+
+        GUILayout.Space(20);
+
+        name = EditorGUILayout.TextField("Card name", name);
+
+        GUILayout.Space(20);
+
+        if(GUILayout.Button("Remove Card")){
+            RemoveCard();
+        }
+    }
+}
--- ClashFPS/Assets/Scripts/Helpers/CardTypes.cs
+++ ClashFPS/Assets/Scripts/Helpers/CardTypes.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public static class CardTypes
 {
-    public const string Wizard = "Wizard";
-    public const string Valkyrie = "Valkyrie";
-
     public static GameObject StringToCardPrefab(string cardName){
         return Resources.Load<GameObject>($"{cardName}/{cardName}Card");
     }
+
+    public const string Wizard = "Wizard";
+    public const string Valkyrie = "Valkyrie";
 }
